9a3595690293e965fb5aa65dbe1a74b1fe954c49
Luiz Gustavo Fagundes Teixeira <luizagustinho032@gmail.com>
2024-05-30 02:27:45 -0300

    Foi adicionado a funcionalidade de localização do endereço do usuário através do CEP na tela de cadastro de denúncia. Os elementos em html e css foram somente reorganizados. Foi adicionada a funcionalidade (ainda não finalizada) de busca de telefones úteis

--- codigo/cadastro-denuncia/assets/cadastro-denun.js
+++ codigo/cadastro-denuncia/assets/cadastro-denun.js
@@ -1,14 +1,20 @@
 const assunto = document.getElementById('assunto');
 const gravidade = document.getElementById('gravidade');
 const região = document.getElementById('região');
-const estado = document.getElementById('estado');
+
+
+const cep = document.querySelector('#cep')
+const endereco = document.querySelector('#endereco')
 const cidade = document.getElementById('cidade');
 const bairro = document.getElementById('bairro');
+
+const estado = document.getElementById('estado');
+
 const descrição= document.getElementById('descrição');
 const foto= document.getElementById('fileInput');
 
 const form = document.querySelector('#forms');
-
+const message = document.getElementById('message');
 
 function le_dados (){
     let dados= localStorage.getItem('db');
@@ -26,9 +32,11 @@ function le_dados (){
                     assunto:"Segurança",
                     gravidade:"Grave",
                     região: "Norte",
-                    estado:"Minas Gerais",
+                    cep:"30290270",
+                    endereco:"Rua Eldorado",
                     cidade:"Belo Horizonte",
                     bairro:"Barreiro",
+                    estado:"Minas Gerais",
                     descrição:"Ocorrida a noite",
                     foto:"img"
                 } 
@@ -50,9 +58,11 @@ function incluir_contato(){
     let strassunto = document.getElementById('assunto').value;
     let strgravidade = document.getElementById('gravidade').value;
     let strregião = document.getElementById('região').value;
-    let strestado = document.getElementById('estado').value;
+    let strcep = document.getElementById('cep').value;
+    let strendereco = document.getElementById('endereco').value;
     let strcidade = document.getElementById('cidade').value;
     let strbrairro = document.getElementById('bairro').value;
+    let strestado = document.getElementById('estado').value;
     let strdescrição = document.getElementById('descrição').value;
     let strfoto = document.getElementById('fileInput');
     //CRIANDO NOVO CONTATO 
@@ -61,9 +71,11 @@ function incluir_contato(){
         assunto:strassunto,
         gravidade:strgravidade,
         região: strregião,
-        estado: strestado,
+        cep:strcep,
+        endereco: strendereco,
         cidade: strcidade,
         bairro: strbrairro,
+        estado: strestado,
         descrição:strdescrição,
         foto:strfoto
     }
@@ -79,6 +91,49 @@ function incluir_contato(){
 
 
 //configurando os botões//
+
+cep.addEventListener('focusout',async() =>{
+    try {
+        const onlyNumbers = /^\d+$/;
+        const cepValid = /^\d{8}$/;
+        
+        if(!onlyNumbers.test(cep.value) || !cepValid.test(cep.value) ){
+            throw{cep_error:'Cep Inválido'};
+    
+        }
+
+        const response = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`);
+
+        if(!response.ok){
+            throw await response.json();
+
+        }
+
+        const responseCep = await response.json();
+        endereco.value =responseCep.logradouro;
+        cidade.value =responseCep.localidade;
+        bairro.value =responseCep.bairro;
+
+
+    } catch (error) { 
+
+        if(error?.cep_error){
+            message.textContent=error.cep_error;
+            setTimeout(()=>{
+                message.textContent="";
+
+            },5000)
+        }
+
+        
+
+
+        
+    }
+    
+
+})
+
 document.getElementById('envio').addEventListener('click',incluir_contato);
 
 
